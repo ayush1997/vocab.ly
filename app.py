0a14e050a411e0e023e31723eca16046c98bbe91
ayush <ayushkumarsingh97@gmail.com>
2016-05-07 22:03:50 +0530

    app.py added

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask import render_template,request,flash,url_for,redirect,session
+import requests
+import os,json
+import random
+
+
+app = Flask(__name__)
+app.secret_key = "secret"
+
+api_keys = ['0c209d14061fe2f68b64e9dfd9453280','88044ee383eef5827ee87f3022224354']
+
+
+@app.route('/api', methods=['GET','POST'])
+def main():
+
+
+
+
+
+
+    return "HI"
+
+
+
+
+
+
+if __name__ == '__main__' :
+	# port = int(os.environ.get('PORT',443))
+	# app.run(host='0.0.0.0',port=port)
+	app.run(debug=True)
